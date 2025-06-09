ff3b1347893af67fd66bb88dad5866664d846d62
Beatriz <biapgom@gmail.com>
2025-06-08 23:06:29 -0300

    Create 11.04.25.EX02.py
    Objetivo: uma função chamada tabuada(n) que recebe um número inteiro e imprime a sua tabuada de 1 a 10.

---
+++ 11.04.25.EX02.py
@@ -0,0 +1,14 @@
+#Beatriz Paulino
+#11/04/25
+#EX02
+
+def tabuada(n):
+    for i in range(1, 11):
+        print(n * i)
+
+def main():
+    n = int(input("Insira um número: "))
+    tabuada(n)
+    
+
+main()
